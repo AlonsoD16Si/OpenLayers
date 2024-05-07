b975764f6962b9fdaf6f562bc4b957d6945d28df
AlonsoD16Si <alonso_lopezsilva@live.com>
2024-05-07 15:01:46 -0600

    ultima parte de video

--- app-ol/src/app/app.component.ts
+++ app-ol/src/app/app.component.ts
@@ -1,16 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, Inject, TemplateRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import  {MapComponent}  from './map/map.component';
+import { MapComponent } from './map/map.component';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+export interface DialogMapaData {
+  wkt: string;
+}
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MapComponent
+    MapComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'app-ol';
+  animal!: string;
+  name!: string;
+
+  constructor(public dialog: MatDialog) { }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogMapa, {
+      data: { wkt: '' },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log("Resultado del dialog", result);
+    });
+  }
+  //form2
+  @ViewChild(MapComponent) map!: MapComponent;
+  abrirMapa(templateRef: TemplateRef<any>): void {
+    this.dialog.open(templateRef, {});
+  }
+  saveMapa(): void {
+console.log("Resultado del dialog: ",this.map.wkt);
+  }
+}
+
+//Componente de dialogo
+@Component({
+  selector: 'dialog-mapa',
+  templateUrl: './dialog-mapa.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MapComponent,
+  ],
+})
+export class DialogMapa {
+  constructor(
+    public dialogRef: MatDialogRef<DialogMapa>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogMapaData,
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  recibir(wkt: any) {
+    console.log("....", wkt);
+    this.data.wkt = wkt;
+  }
 }
--- app-ol/src/app/map/map.component.ts
+++ app-ol/src/app/map/map.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import Map from 'ol/map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { FullScreen, defaults as defaultControls } from 'ol/control';
+
 import LayerSwitcher from 'ol-ext/control/LayerSwitcher';
+import LayerGroup from 'ol/layer/Group.js';
+import XYZ from 'ol/source/XYZ.js';
+
+import Bar from 'ol-ext/control/Bar';
+import VectorLayer from 'ol/layer/Vector.js';
+import VectorSource from 'ol/source/Vector.js';
+import Toggle from 'ol-ext/control/Toggle';
+import Draw from 'ol/interaction/Draw.js';
+
+import WKT from 'ol/format/WKT.js';
 
 @Component({
   selector: 'app-map',
@@ -15,8 +26,29 @@ import LayerSwitcher from 'ol-ext/control/LayerSwitcher';
 })
 export class MapComponent {
   map!: Map;
+  public wkt!: string;
+
+  @Output()
+  enviar: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnInit(): void {
+    let self = this;
+    let lg_capas_base = new LayerGroup({
+      properties: { title: 'Capas Base', openInLayerSwitcher: true },
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+          properties: { title: 'OSM', baseLayer: true }
+        }),
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}'
+          }),
+          properties: { title: 'Google Maps', baseLayer: true },
+          visible: false
+        })
+      ]
+    })
     this.map = new Map({
       controls: defaultControls({
         zoom: true,
@@ -29,16 +61,77 @@ export class MapComponent {
         .extend([new FullScreen({ tipLabel: 'Pantalla completa' })]),
       target: 'map',
       layers: [
-        new TileLayer({
-          source: new OSM()
-        })
+        lg_capas_base
+
       ],
       view: new View({
         center: [0, 0],
         zoom: 2
       })
     });
+
+    // console.log(this.map.getView().getProjection());//Sistema de coordenadas que maneja OpenLayer
     this.map.addControl(new LayerSwitcher());
-    // this.map.addControl(new FullScreen({tipLabel: 'Pantalla completa'}));
+    //manejo de vectores
+    let vectorLayer = new VectorLayer({
+      source: new VectorSource(),//almacenar datos
+      properties: { displayInLayerSwitcher: false }
+    })
+    this.map.addLayer(vectorLayer);
+    //barras de controles
+    let mainBar = new Bar({});
+    this.map.addControl(mainBar);
+
+    let dibujoBar = new Bar({
+      group: true,
+      toggleOne: true
+    });
+    mainBar.addControl(dibujoBar);
+
+    //botones que se activan y se desactivan
+    var draw_p = new Draw({//variable de interaccion
+      type: 'Point',
+      source: vectorLayer.getSource() as VectorSource
+    })
+    let tg_punto = new Toggle({
+      title: 'Dibujar punto',
+      html: '<i class="fa-solid fa-location-dot"></i>',
+      interaction: draw_p
+    })
+
+    //Disparador drawend
+    draw_p.on('drawend', function (e) {
+      console.log('...', e);
+
+      let format = new WKT();
+      let wkt = format.writeFeature(e.feature, { featureProjection: "EPSG:3857", dataProjection: "EPSG:4326" });
+      console.log(wkt);
+      self.enviar.emit(wkt);
+    });
+
+
+    dibujoBar.addControl(tg_punto);
+    //Linea
+
+    let draw_l = new Draw({
+      type: 'LineString',
+      source: vectorLayer.getSource() as VectorSource
+    });
+    let tg_linea = new Toggle({
+      title: 'Dibujar linea',
+      html: '<i class="fa-solid fa-share-nodes"></i>',
+      interaction: draw_l
+    });
+    draw_l.on('drawend', function (e) {
+
+      let format = new WKT();
+      let wkt = format.writeFeature(e.feature, { featureProjection: "EPSG:3857", dataProjection: "EPSG:4326" });
+      console.log(wkt);
+      self.wkt = wkt;
+    });
+
+
+    dibujoBar.addControl(tg_linea);
   }
+
 }
